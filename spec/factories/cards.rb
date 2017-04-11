1ca46567307f1e00052aff53f8c7f7c97f0204b0
Giovanni Alberto <delirable@gmail.com>
2017-04-11 02:48:17 -0500

    Finish card factory

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -1,9 +1,8 @@
 FactoryGirl.define do
   factory :card do
-    card_number "MyString"
-    expiration_month "MyString"
-    expiration_year "MyString"
-    secure_code "MyString"
-    card_token "MyString"
+    card_number      { 16.times.map{rand(10)}.join }
+    expiration_month { '%02d' % (1+rand(12)) }
+    expiration_year  { '%02d' % rand(100) }
+    secure_code      { 16.times.map{rand(10)}.join }
   end
 end
