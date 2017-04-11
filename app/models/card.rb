f563464d09b7fa987c388fb25f942a33b330b754
Giovanni Alberto <delirable@gmail.com>
2017-04-11 02:41:29 -0500

    Finish card model and specs

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,32 @@
 class Card < ApplicationRecord
+  validates :card_number, :expiration_month, :expiration_year, :secure_code, presence: true
+  validates :card_number, length: { maximum: 16 }
+  validates :expiration_month, :expiration_year, length: { maximum: 2 }
+  # TODO add a validator for credit card number, but bank is missing in params
+
+  has_many :charges
+
+  after_validation :tokenize
+
+  scope :not_expired, -> { where('updated_at > ?', 10.minutes.ago) }
+
+  def tokenize
+    return unless is_unique?(self.card_token) # not expired
+    begin
+      keys        = Digest::SHA256.hexdigest("#{secure_code}#{Time.now}")
+      self.secure_code = keys[0...keys.size/2]
+      self.card_token  = keys[keys.size/2..-1]
+    end while(!is_unique?(self.card_token))
+  end
+
+  def to_json(opts={})
+    opts.merge!(:only => [:card_token])
+    super(opts)
+  end
+
+  private
+
+  def is_unique?(card_token)
+    Card.not_expired.where(card_token: card_token).empty?
+  end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:card_number) }
+    it { should validate_presence_of(:expiration_month) }
+    it { should validate_presence_of(:expiration_year) }
+    it { should validate_presence_of(:secure_code) }
+    it { should validate_length_of(:card_number).is_at_most(16) }
+    it { should validate_length_of(:expiration_month).is_at_most(2) }
+    it { should validate_length_of(:expiration_year).is_at_most(2) }
+  end
+
+  it 'valid factory' do
+    card = FactoryGirl.build(:card)
+    expect(card).to be_valid
+  end
+
+  describe 'tokenize' do
+    before(:each) do
+      @card = FactoryGirl.create(:card)
+      @card.tokenize
+    end
+
+    it 'should assign a secure_code' do
+      expect(@card.secure_code).not_to be(nil)
+    end
+
+    it 'should assign a card_token' do
+      expect(@card.card_token).not_to be(nil)
+    end
+  end
+
+  describe 'to_json' do
+    before(:each) do
+      @card = FactoryGirl.create(:card)
+    end
+
+    it 'should contain card_token only' do
+      body = JSON.parse(Card.last.to_json)
+      expect(body.has_key?('card_token')).to be(true)
+      expect(body.size).to eq(1)
+    end
+  end
+
+  describe 'is_unique?(card_token)' do
+    before(:each) do
+      @card = FactoryGirl.create(:card)
+      @card.tokenize
+      @card.save
+    end
+
+    it 'should return false' do
+      result = @card.send(:is_unique?, @card.card_token)
+      expect(result).to eq(false)
+    end
+
+    it 'should return true' do
+      result = @card.send(:is_unique?, 'unique_card_token')
+      expect(result).to eq(true)
+    end
+  end
 end
