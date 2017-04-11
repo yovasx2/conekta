9a8e6bffe6e3bbdc32d831249f05f813b93cd85f
Giovanni Alberto <delirable@gmail.com>
2017-04-11 00:26:23 -0500

    apply migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411031027) do
+ActiveRecord::Schema.define(version: 20170411052223) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,24 @@ ActiveRecord::Schema.define(version: 20170411031027) do
     t.string   "private_key"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "cards", force: :cascade do |t|
+    t.string   "card_number"
+    t.string   "expiration_month"
+    t.string   "expiration_year"
+    t.string   "secure_code"
+    t.string   "card_token"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  create_table "charges", force: :cascade do |t|
+    t.string   "amount"
+    t.integer  "card_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["card_id"], name: "index_charges_on_card_id"
   end
 
 end
