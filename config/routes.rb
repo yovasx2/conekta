cf69a7a4998dcc20a9d4e3119b68f94f6ea86c40
Giovanni Alberto <delirable@gmail.com>
2017-04-11 02:47:00 -0500

    Add routes for accounts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :charges
-  resources :cards
-  resources :accounts
+  post  'accounts', to: 'accounts#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
