f084d667d3f2d8ff6a9532617d48a0fcfb6123e6
Giovanni Alberto <delirable@gmail.com>
2017-04-11 00:24:50 -0500

    Restrict params and delete useless methods

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,19 @@
+class AccountsController < ApplicationController
+
+  # POST /accounts
+  def create
+    @account = Account.new(account_params)
+
+    if @account.save
+      render json: @account, status: :created, location: @account
+    else
+      render json: @account.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Only allow a trusted parameter "white list" through.
+    def account_params
+      params.require(:account).permit(:name)
+    end
+end
